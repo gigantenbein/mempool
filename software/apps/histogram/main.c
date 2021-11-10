78597ff641d48cb281a459c888537ea06df61a5e
Marc Gantenbein <ganmarc@student.ethz.ch>
2021-11-10 10:42:47 +0100

    [software] Fix random number generation
    `rand_r()` stalls mempool when used without a low modulo number when
    using it multithreaded.

--- software/apps/histogram/main.c
+++ software/apps/histogram/main.c
@@ -14,20 +14,14 @@
 #include "runtime.h"
 #include "synchronization.h"
 
-uint32_t hist_bins[NBINS] __attribute__((section(".l1_prio")));;
+void shift_lfsr(uint32_t *lfsr)
+{
+  *lfsr ^= *lfsr >> 7;
+  *lfsr ^= *lfsr << 9;
+  *lfsr ^= *lfsr >> 13;
+}
 
-// uint32_t rand_lim(uint32_t limit, uint32_t seed) {
-  // create well distributed random number from 0 to limit
-
-//   uint32_t divisor = (uint32_t)(RAND_MAX/limit);
-//     uint32_t retval;
-
-//     do { 
-//         retval = rand_r(&seed) / divisor;
-//     } while (retval > limit);
-
-//     return retval;
-// }
+uint32_t hist_bins[NBINS] __attribute__((section(".l1_prio")));
 
 int main() {
   uint32_t core_id = mempool_get_core_id();
@@ -48,28 +42,25 @@ int main() {
   // seed random number generator
   // Take care with concurrent access to std's rand function
 
-
   mempool_barrier(num_cores);
   uint32_t drawn_number = 0;
+  uint32_t init_lfsr = core_id * 42 + 1;
 
-  uint32_t rand_state = core_id + 1;
   uint32_t bin_value = 0;
   mempool_barrier(num_cores);
   mempool_start_benchmark();
   
-  if (core_id < 16){
-    for (int i = 0; i<NDRAWS; i++){
-      // rand_r is threadsafe in comparison to rand()
-      // needs seed as pointer
-      drawn_number =(uint32_t) rand_r(&rand_state) % NBINS;
+  for (int i = 0; i<NDRAWS; i++){
+    // rand_r is threadsafe in comparison to rand()
+    // needs seed as pointer
+    shift_lfsr(&init_lfsr);
+    drawn_number = init_lfsr % NBINS;
 
-      lr_sc_add((hist_bins + drawn_number), 1);               
-
-      // do {
-      //   bin_value = load_reserved((hist_bins + drawn_number)) + 1;
-      // } while(store_conditional((hist_bins+drawn_number), bin_value));
-    }
+    do {
+      bin_value = load_reserved((hist_bins + drawn_number)) + 1;
+    } while(store_conditional((hist_bins+drawn_number), bin_value));
   }
+
   mempool_stop_benchmark();
 
   mempool_barrier(num_cores);
