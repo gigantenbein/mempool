2c8d2152c19002cad62cc42f43fafda1d22586e5
Marc Gantenbein <ganmarc@student.ethz.ch>
2021-12-16 11:43:37 +0100

    [hardware] Test distributed LRWait

--- hardware/tb/tcdm_adapter/wave_tcdm.tcl
+++ hardware/tb/tcdm_adapter/wave_tcdm.tcl
@@ -9,6 +9,11 @@ add wave -noupdate -group DUT /tcdm_adapter_tb/*
 
 add wave -noupdate -group TCDMAdapter /tcdm_adapter_tb/dut/*
 
+add wave -noupdate -group QNODE0 /tcdm_adapter_tb/gen_qnodes[0]/i_lrwait_qnode/*
+add wave -noupdate -group QNODE1 /tcdm_adapter_tb/gen_qnodes[1]/i_lrwait_qnode/*
+add wave -noupdate -group QNODE2 /tcdm_adapter_tb/gen_qnodes[2]/i_lrwait_qnode/*
+add wave -noupdate -group QNODE3 /tcdm_adapter_tb/gen_qnodes[3]/i_lrwait_qnode/*
+
 add wave -noupdate -group MEMBANK /tcdm_adapter_tb/mem_bank/*
 
 add wave -noupdate -group FALLTHROUGH /tcdm_adapter_tb/dut/i_rdata_register/*
